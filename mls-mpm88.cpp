d7b99aba25a996c81b6b44498014f67d54f99707
Yuanming Hu <yuanmhu@gmail.com>
2018-08-17 22:07:39 -0400

    updated mls-mpm88.cpp

--- mls-mpm88.cpp
+++ mls-mpm88.cpp
@@ -14,9 +14,9 @@ struct Particle { Vec x, v; Mat F, C; real Jp;
 std::vector<Particle> particles;
 Vector3 grid[n + 1][n + 1];  // velocity + mass, node res = cell res + 1
 
-void advance(real dt) {                // Simulation
-  std::memset(grid, 0, sizeof(grid));  // Reset grid
-  for (auto &p : particles) {          // P2G
+void advance(real dt) {
+  std::memset(grid, 0, sizeof(grid));                              // Reset grid
+  for (auto &p : particles) {                                             // P2G
     Vector2i base_coord = (p.x*inv_dx-Vec(0.5_f)).cast<int>();
     Vec fx = p.x * inv_dx - base_coord.cast<real>();
     // Quadratic kernels, see http://mpm.graphics Formula (123)
@@ -24,24 +24,24 @@ void advance(real dt) {                // Simulation
              Vec(0.5) * sqr(fx - Vec(0.5))};
     auto e = std::exp(hardening * (1.0_f - p.Jp)), mu=mu_0*e, lambda=lambda_0*e;
     real J = determinant(p.F);         //                         Current volume
-    Mat r, s; polar_decomp(p.F, r, s); //Polor decomp. for Fixed Corotated Model
-    auto force =                   // Negative Cauchy stress times dt and inv_dx
-        inv_dx*dt*vol*(2*mu * (p.F-r) * transposed(p.F) + lambda * (J-1) * J);
+    Mat r, s; polar_decomp(p.F, r, s); //Polor decomp. for fixed corotated model
+    auto stress =                           // Cauchy stress times dt and inv_dx
+        -inv_dx*dt*vol*(2*mu * (p.F-r) * transposed(p.F) + lambda * (J-1) * J);
     for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) { // Scatter to grid
-        auto dpos = fx - Vec(i, j);
+        auto dpos = Vec(i, j) - fx;
         Vector3 contrib(p.v * particle_mass, particle_mass);
         grid[base_coord.x + i][base_coord.y + j] +=
-            w[i].x*w[j].y*(contrib+Vector3(4.0_f*(force+p.C*particle_mass)*dpos));
+          w[i].x*w[j].y*(contrib+Vector3(4.0_f*(stress+p.C*particle_mass)*dpos));
       }
   }
   for(int i = 0; i <= n; i++) for(int j = 0; j <= n; j++) { //For all grid nodes
       auto &g = grid[i][j];
-      if (g[2] > 0) {                                  // No need for epsilon here
-        g /= g[2];                                     //        Normalize by mass
-        g += dt * Vector3(0, -100, 0);                 //                  Gravity
-        real boundary=0.05,x=(real)i/n,y=real(j)/n;//boundary thickness,node coord
-        if (x < boundary||x > 1-boundary||y > 1-boundary) g=Vector3(0);//Sticky BC
-        if (y < boundary) g[1]=std::max(0.0_f, g[1]);              //"Separate" BC
+      if (g[2] > 0) {                                // No need for epsilon here
+        g /= g[2];                                   //        Normalize by mass
+        g += dt * Vector3(0, -100, 0);               //                  Gravity
+        real boundary=0.05,x=(real)i/n,y=real(j)/n; //boundary thick.,node coord
+        if (x < boundary||x > 1-boundary||y > 1-boundary) g=Vector3(0); //Sticky
+        if (y < boundary) g[1] = std::max(0.0_f, g[1]);             //"Separate"
       }
     }
   for (auto &p : particles) { // Grid to particle
@@ -51,14 +51,14 @@ void advance(real dt) {                // Simulation
              Vec(0.5) * sqr(fx - Vec(0.5))};
     p.C = Mat(0); p.v = Vec(0);
     for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) {
-        auto dpos = fx - Vec(i, j),
+        auto dpos = Vec(i, j) - fx,
             grid_v = Vec(grid[base_coord.x + i][base_coord.y + j]);
         auto weight = w[i].x * w[j].y;
-        p.v += weight * grid_v;                                        // Velocity
+        p.v += weight * grid_v;                                      // Velocity
         p.C += Mat::outer_product(weight * grid_v, dpos);              // APIC C
       }
     p.x += dt * p.v;                                                // Advection
-    auto F = (Mat(1) - (4 * inv_dx * dt) * p.C) * p.F;       // MLS-MPM F-update
+    auto F = (Mat(1) + (4 * inv_dx * dt) * p.C) * p.F;       // MLS-MPM F-update
     Mat svd_u, sig, svd_v; svd(F, svd_u, sig, svd_v);
     for (int i = 0; i < 2; i++)                               // Snow Plasticity
       sig[i][i] = clamp(sig[i][i], 1.0_f - 2.5e-2_f, 1.0_f + 7.5e-3_f);
@@ -68,21 +68,21 @@ void advance(real dt) {                // Simulation
   }
 }
 
-void add_object(Vec center) {    // Seed particles
+void add_object(Vec center) {                                  // Seed particles
   for (int i = 0; i < 1000; i++) // Randomly sample 1000 particles in the square
-    particles.push_back(Particle((Vec::rand()*2.0_f-Vec(1))*0.08_f+center));  }
-
+    particles.push_back(Particle((Vec::rand()*2.0_f-Vec(1))*0.08_f+center));
+}
 int main() {
   GUI gui("Taichi Demo: Real-time MLS-MPM 2D ", window_size, window_size);
   add_object(Vec(0.5,0.4));add_object(Vec(0.45,0.6));add_object(Vec(0.55,0.8));
-  for (int i = 0;; i++) {                              // Main Loop
-    advance(dt);                                       // Advance simulation
-    if (i % int(frame_dt / dt) == 0) {                 // Redraw frame
+  for (int i = 0;; i++) {                              //              Main Loop
+    advance(dt);                                       //     Advance simulation
+    if (i % int(frame_dt / dt) == 0) {                 //           Redraw frame
       gui.get_canvas().clear(Vector4(0.2, 0.4, 0.7, 1.0_f)); // Clear background
       for (auto p : particles)                         // Draw particles
         gui.buffer[(p.x * (inv_dx*window_size/n)).cast<int>()] = Vector4(0.8);
       gui.update();                                    // Update image
     }//Reference: A Moving Least Squares Material Point Method with Displacement
-  } //           Discontinuity and Two-Way Rigid Body Coupling (SIGGRAPH 2018)
+  } //             Discontinuity and Two-Way Rigid Body Coupling (SIGGRAPH 2018)
 }  //  By Yuanming Hu (who also wrote this 88-line version), Yu Fang, Ziheng Ge,
 //                          Ziyin Qu, Yixin Zhu, Andre Pradhana, Chenfanfu Jiang
